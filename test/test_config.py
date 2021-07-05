99dd859c036326a80ffd248861ea843f30144239
John T. Wodder II <git@varonathe.org>
2021-07-05 17:18:12 -0400

    Test config parsing errors

--- test/test_config.py
+++ test/test_config.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from typing import Any, Dict
 import pytest
 from versioningit.config import Config
+from versioningit.errors import ConfigError, NotVersioningitError
 
 DATA_DIR = Path(__file__).with_name("data")
 
@@ -17,3 +18,14 @@ def test_parse_toml_file(tomlfile: Path) -> None:
     namespace: Dict[str, Any] = {}
     exec(tomlfile.with_suffix(".py").read_text(encoding="utf-8"), namespace)
     assert cfg == namespace["cfg"]
+
+
+@pytest.mark.parametrize(
+    "tomlfile",
+    sorted((DATA_DIR / "config-error").glob("*.toml")),
+    ids=attrgetter("stem"),
+)
+def test_parse_bad_toml_file(tomlfile: Path) -> None:
+    with pytest.raises((ConfigError, NotVersioningitError)) as excinfo:
+        Config.parse_toml_file(tomlfile)
+    assert str(excinfo.value) == tomlfile.with_suffix(".txt").read_text().strip()
