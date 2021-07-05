309af03d2ac7433fffddc68a8b0db9f7083cdf71
John T. Wodder II <git@varonathe.org>
2021-07-05 19:25:45 -0400

    Test writing version to files

--- test/test_end2end.py
+++ test/test_end2end.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import shutil
 import subprocess
 import sys
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, Field
 import pytest
 from versioningit.core import get_version, get_version_from_pkg_info
@@ -12,9 +12,17 @@ from versioningit.util import parse_version_from_metadata
 DATA_DIR = Path(__file__).with_name("data")
 
 
+class WriteFile(BaseModel):
+    sdist_path: str
+    wheel_path: str
+    contents: str
+    encoding: Optional[str]
+
+
 class CaseDetails(BaseModel):
     version: str
     local_modules: List[str] = Field(default_factory=list)
+    write_file: Optional[WriteFile] = None
 
 
 @pytest.mark.skipif(shutil.which("git") is None, reason="Git not installed")
@@ -44,9 +52,17 @@ def test_end2end_git(repozip: Path, tmp_path: Path) -> None:
     shutil.unpack_archive(str(sdist), str(tmp_path / "sdist"))
     (sdist_src,) = (tmp_path / "sdist").iterdir()
     assert get_version_from_pkg_info(sdist_src) == details.version
+    if details.write_file is not None:
+        assert (sdist_src / details.write_file.sdist_path).read_text(
+            encoding=details.write_file.encoding
+        ) == details.write_file.contents
 
     (wheel,) = (srcdir / "dist").glob("*.whl")
     shutil.unpack_archive(str(wheel), str(tmp_path / "wheel"), "zip")
     (wheel_dist_info,) = (tmp_path / "wheel").glob("*.dist-info")
     metadata = (wheel_dist_info / "METADATA").read_text(encoding="utf-8")
     assert parse_version_from_metadata(metadata) == details.version
+    if details.write_file is not None:
+        assert (tmp_path / "wheel" / details.write_file.wheel_path).read_text(
+            encoding=details.write_file.encoding
+        ) == details.write_file.contents
